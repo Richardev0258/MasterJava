d0eaa4e49a9a0afd5d3d3528ac3d7170b4832646
Ricardo Andrés Marín Pinilla <ricardomarin0258@gmail.com>
2025-07-03 15:17:12 -0500

    init

---
+++ src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args){
+        String saludar;
+        saludar = "Hola Mundo desde Java!";
+        System.out.println(saludar);
+    }
+}

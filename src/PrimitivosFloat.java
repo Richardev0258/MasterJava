a1fed816e0cd437031e8edfbafd5b126ffb386a8
Ricardo Andrés Marín Pinilla <ricardomarin0258@gmail.com>
2025-07-03 17:05:32 -0500

    varianbles

--- src/PrimitivosFloat.java
+++ src/PrimitivosFloat.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class PrimitivosFloat {
+    public static void main(String[] arg){
+
+        float realFloat = 3.4028235E38F;
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("tipo float corresponde en byte a " + Float.BYTES);
+        System.out.println("tipo float corresponde en bites a " + Float.SIZE);
+        System.out.println("valor máximo de un float: " + Float.MAX_VALUE);
+        System.out.println("valor mínimo de un float: " + Float.MIN_VALUE);
+
+        double numeroDouble = 1.7976931348623157E308;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("tipo double corresponde en byte a " + Double.BYTES);
+        System.out.println("tipo double corresponde en bites a " + Double.SIZE);
+        System.out.println("valor máximo de un double: " + Double.MAX_VALUE);
+        System.out.println("valor mínimo de un double: " + Double.MIN_VALUE);
+    }
 }

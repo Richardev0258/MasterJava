b58f8c84614d2d262c64ea7bb7611d79cc526dc6
Ricardo Andrés Marín Pinilla <ricardomarin0258@gmail.com>
2025-07-07 13:33:26 -0500

    Sistemas Num

--- src/SistemasNumericos.java
+++ src/SistemasNumericos.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class SistemasNumericos {
+    public static void main(String[] args) {
+        int numeroDecimal = 500;
+
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        System.out.println("número binario de " + numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+        int numeroBinario = 0b111110100;
+        System.out.println("numeroBinario = " + numeroBinario);
+
+        System.out.println("número octal de = " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal));
+        int numeroOctal = 0764;
+        System.out.println("numeroOctal = " + numeroOctal);
+
+        System.out.println("número hexadecimal de " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal));
+        int numeroHex = 0x1f4;
+        System.out.println("numeroHex = " + numeroHex);
+    }
 }
